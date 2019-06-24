6bc9b70e9429865a526ca51c1fbf2a4ba82d0452
christian <christian.phu@fr.clara.net>
2019-06-24 10:43:10 +0200

    AZ-106_add_locals_tf

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-locals {
-  vnet_src_parts               = "${split("/", var.vnet_src_id)}"
-  vnet_src_name                = "${element(local.vnet_src_parts, 8)}"
-  vnet_src_subscription_id     = "${element(local.vnet_src_parts, 2)}"
-  vnet_src_resource_group_name = "${element(local.vnet_src_parts, 4)}"
-
-  vnet_dest_parts               = "${split("/", var.vnet_dest_id)}"
-  vnet_dest_name                = "${element(local.vnet_dest_parts, 8)}"
-  vnet_dest_subscription_id     = "${element(local.vnet_dest_parts, 2)}"
-  vnet_dest_resource_group_name = "${element(local.vnet_dest_parts, 4)}"
-}
-
 data "azurerm_client_config" "current_config" {}
 
 provider "azurerm" {
