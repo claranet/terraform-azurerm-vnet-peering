6b915ebb86a068b720a6ea9a65fa74f7d3b9012e
Jeremy MARMOL <jeremy.marmol@fr.clara.net>
2024-12-16 10:04:00 +0100

    chore(AZ-1088): apply suggestion

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
-  src_parts               = provider::azapi::parse_resource_id("Microsoft.Network/virtualNetworks", var.src_virtual_network_id)
-  src_name                = local.src_parts.name
-  src_resource_group_name = local.src_parts.resource_group_name
+  parsed_src              = provider::azapi::parse_resource_id("Microsoft.Network/virtualNetworks", var.src_virtual_network_id)
+  src_name                = local.parsed_src.name
+  src_resource_group_name = local.parsed_src.resource_group_name
 
-  dest_parts               = provider::azapi::parse_resource_id("Microsoft.Network/virtualNetworks", var.dest_virtual_network_id)
-  dest_name                = local.dest_parts.name
-  dest_resource_group_name = local.dest_parts.resource_group_name
+  parsed_dest              = provider::azapi::parse_resource_id("Microsoft.Network/virtualNetworks", var.dest_virtual_network_id)
+  dest_name                = local.parsed_dest.name
+  dest_resource_group_name = local.parsed_dest.resource_group_name
 }
